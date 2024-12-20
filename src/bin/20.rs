ef47d62cbfe996de418ccb1e1ffec559e20afe94
Dave <dave@dhills.net>
2024-12-20 15:39:31 +0000

    Day 20

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -85,41 +85,68 @@ fn successors(node: &Node, map: &HashSet<Position>) -> Vec<(Node, usize)> {
     result
 }
 
-fn find_best_path(map: &HashSet<Position>, start: Node, end: &Position) -> Option<Vec<Node>> {
+fn find_path(map: &HashSet<Position>, start: Node, end: &Position) -> Option<Vec<Node>> {
     let result = astar(
         &start,
         |node| successors(node, map),
-        |_| 0, // Don't care about the heuristic
+        |_| 0,
         |node| node.position == *end,
     );
-    result.map(|(path, _)| path)
+    result.map(|x| x.0)
 }
 
-fn try_cheat(
+fn locations_n_away(
     map: &HashSet<Position>,
-    start: Node,
-    end: &Position,
+    start: &Position,
+    n: usize,
     max: &Position,
-    cache: &mut HashMap<Node, Vec<usize>>,
-) -> Vec<usize> {
-    // From a given start point, return the best paths
-    // that start from a wall position next to our start point
-    //
-    if let Some(cached) = cache.get(&start) {
-        return cached.clone();
-    }
-    let possible_moves = move_options(&start, max);
+) -> Vec<Position> {
+    // Return a list of all positions that are on a path and have a manhattan distance of n
+    // from the starting point
     let mut result = Vec::new();
-    for next_move in possible_moves {
-        if map.contains(&next_move.position) {
-            // We can cheat from here
-            let path = find_best_path(map, next_move, end);
-            if let Some(path) = path {
-                result.push(path.len());
+    for x in 0..=max.x {
+        for y in 0..=max.y {
+            let pos = Position { x, y };
+            if map.contains(&pos) {
+                continue;
+            }
+            if (start.x as isize - pos.x as isize).abs() + (start.y as isize - pos.y as isize).abs()
+                == n as isize
+            {
+                result.push(pos);
             }
         }
     }
-    cache.insert(start, result.clone());
+    result
+}
+
+fn try_cheat_distance(
+    map: &HashSet<Position>,
+    start: &Node,
+    path: &[Node],
+    path_hash: &HashMap<Node, usize>,
+    cheat_distance: usize,
+    max: &Position,
+) -> Vec<usize> {
+    let possible_positions = locations_n_away(map, &start.position, cheat_distance, max);
+    let mut result = Vec::new();
+    let length = path.len();
+    let current_distance_to_end = length - path.iter().position(|x| x == start).unwrap();
+
+    for pos in possible_positions {
+        let node = Node { position: pos };
+        if path_hash.contains_key(&node) {
+            let path_posn = path_hash[&node];
+            let distance_to_end = length - path_posn + cheat_distance;
+            // println!(
+            //     "Start: {:?}, Path: {:?}, Cheat: {:?}, Distance: {}, Current: {}",
+            //     start.position, path_posn, pos, distance_to_end, current_distance_to_end
+            // );
+            if distance_to_end < current_distance_to_end {
+                result.push(current_distance_to_end - distance_to_end);
+            }
+        }
+    }
     result
 }
 
@@ -128,46 +155,58 @@ fn try_all_cheats_along_path(
     path: Vec<Node>,
     end: &Position,
     max: &Position,
-    cache: &mut HashMap<Node, Vec<usize>>,
+    max_cheat_distance: usize,
 ) -> Vec<usize> {
-    // Given a valid path, try starting from each point in turn and cheating from that point
-    // Return the best path length for each of those starting points
-    path.iter()
+    let mut result = Vec::new();
+    let path_hash_set: HashMap<Node, usize> = path
+        .iter()
         .enumerate()
-        .flat_map(|(i, node)| {
-            try_cheat(map, *node, end, max, cache)
-                .into_iter()
-                .map(|x| x + i)
-                .filter(|x| *x <= path.len()) // Only consider paths that are shorter than the
-                // original
-                // .inspect(|x| println!("{}: {}", i, x))
-                .collect::<Vec<usize>>()
-        })
-        .collect()
+        .map(|(i, node)| (*node, i))
+        .collect();
+    for node in path.iter() {
+        if node.position == *end {
+            break;
+        }
+        for cheat_distance in 2..=max_cheat_distance {
+            let cheats = try_cheat_distance(map, node, &path, &path_hash_set, cheat_distance, max);
+            result.extend(cheats);
+        }
+    }
+    result
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
     let (map, start, end) = input_to_map(input);
     let start = Node { position: start };
+    let path_without_cheating = find_path(&map, start, &end).unwrap();
     let max = map
         .iter()
         .fold(Position { x: 0, y: 0 }, |acc, pos| Position {
             x: acc.x.max(pos.x),
             y: acc.y.max(pos.y),
         });
-    let path_without_cheating = find_best_path(&map, start, &end)?;
-    let len_path_without_cheating = path_without_cheating.len() - 1;
-    println!("Path without cheating: {}", len_path_without_cheating);
-    let mut cache = HashMap::new();
-    let cheat_paths =
-        try_all_cheats_along_path(&map, path_without_cheating, &end, &max, &mut cache);
+    let cheat_path_savings = try_all_cheats_along_path(&map, path_without_cheating, &end, &max, 2);
+    let target = 100;
+    let result = cheat_path_savings.iter().filter(|x| **x >= target).count();
 
-    let count = cheat_paths.len();
-    Some(count)
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let (map, start, end) = input_to_map(input);
+    let start = Node { position: start };
+    let path_without_cheating = find_path(&map, start, &end).unwrap();
+    let max = map
+        .iter()
+        .fold(Position { x: 0, y: 0 }, |acc, pos| Position {
+            x: acc.x.max(pos.x),
+            y: acc.y.max(pos.y),
+        });
+    let cheat_path_savings = try_all_cheats_along_path(&map, path_without_cheating, &end, &max, 20);
+    let target = 100;
+    let result = cheat_path_savings.iter().filter(|x| **x >= target).count();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -179,7 +218,7 @@ mod tests {
         let input = &advent_of_code::template::read_file("examples", DAY);
         let (map, start, end) = input_to_map(input);
         let start = Node { position: start };
-        let path_without_cheating = find_best_path(&map, start, &end).unwrap();
+        let path_without_cheating = find_path(&map, start, &end).unwrap();
         let len_path_without_cheating = path_without_cheating.len() - 1;
 
         assert_eq!(len_path_without_cheating, 84);
@@ -190,9 +229,7 @@ mod tests {
         let cheat_start = Node {
             position: cheat_posn,
         };
-        let cheat_path = find_best_path(&map, cheat_start, &end).unwrap();
-        let len_cheat_path = cheat_path.len() + 12;
-        assert_eq!(len_cheat_path - 1, 72);
+
         let max = map
             .iter()
             .fold(Position { x: 0, y: 0 }, |acc, pos| Position {
@@ -200,34 +237,70 @@ mod tests {
                 y: acc.y.max(pos.y),
             });
 
-        let all_cheat_paths = try_all_cheats_along_path(&map, path_without_cheating, &end, &max);
-        let savings = vec![
-            //(time_saved, number_of_ways_to achieve)
-            (2, 14),
-            (4, 14),
-            (6, 2),
-            (8, 4),
-            (10, 2),
-            (12, 3),
-            (20, 1),
-            (36, 1),
-            (38, 1),
-            (40, 1),
-            (64, 1),
-        ];
-        for (saving, expected_count) in savings {
-            let count = all_cheat_paths
-                .iter()
-                .map(|x| len_path_without_cheating - x)
-                .filter(|x| *x == saving)
-                .count();
-            assert_eq!(count, expected_count);
-        }
+        let path_hash: HashMap<Node, usize> = path_without_cheating
+            .iter()
+            .enumerate()
+            .map(|(i, node)| (*node, i))
+            .collect();
+        let cheat_path = try_cheat_distance(
+            &map,
+            &cheat_start,
+            &path_without_cheating,
+            &path_hash,
+            2,
+            &max,
+        );
+        assert_eq!(cheat_path, vec![12]);
+
+        let all_cheat_paths = try_all_cheats_along_path(&map, path_without_cheating, &end, &max, 2);
+        assert_eq!(all_cheat_paths.len(), 44);
+
+        // dbg!(&all_cheat_paths);
+
+        //let savings = vec![
+        //    //(time_saved, number_of_ways_to achieve)
+        //    (2, 14),
+        //    (4, 14),
+        //    (6, 2),
+        //    (8, 4),
+        //    (10, 2),
+        //    (12, 3),
+        //    (20, 1),
+        //    (36, 1),
+        //    (38, 1),
+        //    (40, 1),
+        //    (64, 1),
+        //];
+        // for (saving, expected_count) in savings {
+        //     let count = all_cheat_paths
+        //         .iter()
+        //         .map(|x| len_path_without_cheating - x)
+        //         .filter(|x| *x == saving)
+        //         .count();
+        //     assert_eq!(count, expected_count);
+        // }
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_locations_n_away() {
+        let input = &advent_of_code::template::read_file("examples", DAY);
+        let (map, start, _) = input_to_map(input);
+        let max = map
+            .iter()
+            .fold(Position { x: 0, y: 0 }, |acc, pos| Position {
+                x: acc.x.max(pos.x),
+                y: acc.y.max(pos.y),
+            });
+        let result = locations_n_away(&map, &start, 2, &max);
+        assert_eq!(result.len(), 2);
+        let other_posn = Position { x: 5, y: 3 };
+        let result = locations_n_away(&map, &other_posn, 2, &max);
+        assert_eq!(result.len(), 3);
     }
 }
