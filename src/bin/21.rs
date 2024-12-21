05f498611c0b7b1f81a6b6c15a114bd266aacf02
Dave <dave@dhills.net>
2024-12-21 15:29:01 +0000

    Day 21

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,314 +1,198 @@
 advent_of_code::solution!(21);
+use std::collections::{HashMap, VecDeque};
+use std::fs;
+use std::rc::Rc;
+use std::time::Instant;
 
-use itertools::Itertools;
+pub const DIRS: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Action {
-    Up,
-    Down,
-    Left,
-    Right,
-    Activate,
-}
+const NUMERIC: [[u8; 3]; 4] = [
+    [b'7', b'8', b'9'],
+    [b'4', b'5', b'6'],
+    [b'1', b'2', b'3'],
+    [b' ', b'0', b'A'],
+];
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Button {
-    One,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Zero,
-    Activate,
-}
+const DIRECTIONAL: [[u8; 3]; 2] = [[b' ', b'^', b'A'], [b'<', b'v', b'>']];
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Position {
-    x: usize,
-    y: usize,
-}
-
-fn position_for_button(button: &Button) -> Position {
-    match button {
-        Button::One => Position { x: 0, y: 2 },
-        Button::Two => Position { x: 1, y: 2 },
-        Button::Three => Position { x: 2, y: 2 },
-        Button::Four => Position { x: 0, y: 1 },
-        Button::Five => Position { x: 1, y: 1 },
-        Button::Six => Position { x: 2, y: 1 },
-        Button::Seven => Position { x: 0, y: 0 },
-        Button::Eight => Position { x: 1, y: 0 },
-        Button::Nine => Position { x: 2, y: 0 },
-        Button::Zero => Position { x: 1, y: 3 },
-        Button::Activate => Position { x: 2, y: 3 },
+fn find_shortest_paths(
+    keypad: &[[u8; 3]],
+    from: u8,
+    to: u8,
+    cache: &mut HashMap<(u8, u8), Rc<Vec<Vec<u8>>>>,
+) -> Rc<Vec<Vec<u8>>> {
+    if let Some(cached) = cache.get(&(from, to)) {
+        return cached.clone();
     }
-}
 
-fn position_for_action(action: &Action) -> Position {
-    match action {
-        Action::Up => Position { x: 1, y: 0 },
-        Action::Down => Position { x: 1, y: 1 },
-        Action::Left => Position { x: 0, y: 1 },
-        Action::Right => Position { x: 2, y: 1 },
-        Action::Activate => Position { x: 2, y: 0 },
+    if from == to {
+        let result = Rc::new(vec![vec![b'A']]);
+        cache.insert((from, to), result.clone());
+        return result;
     }
-}
 
-fn get_path_combinations(horizontal: Vec<Action>, vertical: Vec<Action>) -> Vec<Vec<Action>> {
-    let total_len = horizontal.len() + vertical.len();
-
-    // We only need to choose the positions for one set of moves
-    // The other set will fill in the gaps
-    (0..total_len)
-        .combinations(horizontal.len())
-        .map(|positions| {
-            let mut result = vec![vertical[0].clone(); total_len]; // Fill with vertical moves
-            for (i, pos) in positions.iter().enumerate() {
-                result[*pos] = horizontal[i].clone(); // Put horizontal moves in chosen positions
+    // find 'from' and 'to' on keypad
+    let mut start = (0, 0);
+    let mut end = (0, 0);
+    for (y, row) in keypad.iter().enumerate() {
+        for (x, &c) in row.iter().enumerate() {
+            if c == from {
+                start = (x, y);
             }
-            result
-        })
-        .collect()
-}
-
-fn path_includes_position(path: &[Action], start: &Position, pos: &Position) -> bool {
-    // Return true if moving from start via the steps given in path would include the position pos
-    let mut current = *start;
-    for step in path {
-        let next = match step {
-            Action::Up => Position {
-                x: current.x,
-                y: current.y - 1,
-            },
-            Action::Down => Position {
-                x: current.x,
-                y: current.y + 1,
-            },
-            Action::Left => Position {
-                x: current.x - 1,
-                y: current.y,
-            },
-            Action::Right => Position {
-                x: current.x + 1,
-                y: current.y,
-            },
-            Action::Activate => current,
-        };
-        if next == *pos {
-            return true;
-        }
-        current = next;
-    }
-    false
-}
-
-fn ways_to_reach_button(current: &Button, target: &Button) -> Vec<Vec<Action>> {
-    // Given a start and an end button, return all possible ways to reach the target button
-    // excluding any that pass through the empty space at 0,3
-    let current_position = position_for_button(current);
-    let target_position = position_for_button(target);
-    let dx = target_position.x as isize - current_position.x as isize;
-    let dy = target_position.y as isize - current_position.y as isize;
-    let horizontal_moves = if dx < 0 {
-        vec![Action::Left; dx.unsigned_abs()]
-    } else {
-        vec![Action::Right; dx.unsigned_abs()]
-    };
-    let vertical_moves = if dy < 0 {
-        vec![Action::Up; dy.unsigned_abs()]
-    } else {
-        vec![Action::Down; dy.unsigned_abs()]
-    };
-    let result = get_path_combinations(horizontal_moves, vertical_moves);
-
-    dbg!(&result);
-    let danger_position = Position { x: 0, y: 3 };
-    result
-        .into_iter()
-        .filter(|path| !path_includes_position(path, &current_position, &danger_position))
-        .collect()
-}
-
-fn actions_to_next_digit(current: &Button, target: &Button) -> Vec<Action> {
-    // Given we are hovering over the current button
-    // Return a vector of optimal vectors of actions to trigger the target button
-    // There may be multiple ways to trigger the target button but we just pick one
-    // that avoids the empty space ad 0,3
-    let current_position = position_for_button(current);
-    let target_position = position_for_button(target);
-
-    let dx = target_position.x as isize - current_position.x as isize;
-    let dy = target_position.y as isize - current_position.y as isize;
-    let mut result = Vec::new();
-    match (dx < 0, dy < 0) {
-        (true, true) => {
-            // Moving left and up
-            // So need to take care not to move through the empty space
-            // But we'd prefer to move left first if possible (this is more eficient)
-            if current_position.y < 3 {
-                result.extend(vec![Action::Left; dx.unsigned_abs()]);
-                result.extend(vec![Action::Up; dy.unsigned_abs()]);
-            } else {
-                result.extend(vec![Action::Up; dy.unsigned_abs()]);
-                result.extend(vec![Action::Left; dx.unsigned_abs()]);
+            if c == to {
+                end = (x, y);
             }
         }
-        (true, false) => {
-            // Moving left and down
-            result.extend(vec![Action::Down; dy.unsigned_abs()]);
-            result.extend(vec![Action::Left; dx.unsigned_abs()]);
-        }
-        (false, true) => {
-            // Moving right and up
-            result.extend(vec![Action::Up; dy.unsigned_abs()]);
-            result.extend(vec![Action::Right; dx.unsigned_abs()]);
-        }
-        (false, false) => {
-            // Moving right and down so need to move right first and then down
-            // Again, we prefer to move down first if possible
-            if target_position.y < 3 {
-                result.extend(vec![Action::Down; dy.unsigned_abs()]);
-                result.extend(vec![Action::Right; dx.unsigned_abs()]);
-            } else {
-                result.extend(vec![Action::Right; dx.unsigned_abs()]);
-                result.extend(vec![Action::Down; dy.unsigned_abs()]);
+    }
+
+    // flood fill keypad to find the shortest distances
+    let mut dists = vec![[usize::MAX; 3]; keypad.len()];
+    let mut queue = VecDeque::new();
+    queue.push_back((start.0, start.1, 0));
+
+    while let Some((x, y, steps)) = queue.pop_front() {
+        dists[y][x] = steps;
+        for (dx, dy) in DIRS {
+            let nx = x as i32 + dx;
+            let ny = y as i32 + dy;
+            if nx >= 0
+                && ny >= 0
+                && nx < 3
+                && ny < keypad.len() as i32
+                && keypad[ny as usize][nx as usize] != b' '
+                && dists[ny as usize][nx as usize] == usize::MAX
+            {
+                queue.push_back((nx as usize, ny as usize, steps + 1));
             }
         }
-    };
+    }
 
-    // However, we need to press the activate button at the end
-    result.push(Action::Activate);
-    result
-}
-
-fn actions_for_next_action(current: &Action, target: &Action) -> Vec<Action> {
-    // Given we are hovering over our current action button
-    // Return a vector of optimal actions to get us to our next action
-    let dx = position_for_action(target).x as isize - position_for_action(current).x as isize;
-    let dy = position_for_action(target).y as isize - position_for_action(current).y as isize;
-    let mut result = vec![];
-    match (dx < 0, dy < 0) {
-        (true, true) => {
-            // Moving left and up
-            result.extend(vec![Action::Up; dy.unsigned_abs()]);
-            result.extend(vec![Action::Left; dx.unsigned_abs()]);
+    // backtrack from 'end' back to 'start' and collect all paths
+    let mut paths = Vec::new();
+    let mut stack = Vec::new();
+    stack.push((end.0, end.1, vec![b'A']));
+    while let Some((x, y, path)) = stack.pop() {
+        if x == start.0 && y == start.1 {
+            paths.push(path);
+            continue;
         }
-        (true, false) => {
-            // Moving left and down
-            result.extend(vec![Action::Down; dy.unsigned_abs()]);
-            result.extend(vec![Action::Left; dx.unsigned_abs()]);
+        for (i, (dx, dy)) in DIRS.iter().enumerate() {
+            let nx = x as i32 + dx;
+            let ny = y as i32 + dy;
+            if nx >= 0
+                && ny >= 0
+                && nx < 3
+                && ny < keypad.len() as i32
+                && dists[ny as usize][nx as usize] < dists[y][x]
+            {
+                // do everything in reverse
+                let c = match i {
+                    0 => b'<',
+                    1 => b'^',
+                    2 => b'>',
+                    3 => b'v',
+                    _ => panic!(),
+                };
+                let mut new_path = vec![c];
+                new_path.extend(&path);
+                stack.push((nx as usize, ny as usize, new_path));
+            }
         }
-        (false, true) => {
-            // Moving right and up
-            result.extend(vec![Action::Right; dx.unsigned_abs()]);
-            result.extend(vec![Action::Up; dy.unsigned_abs()]);
+    }
+
+    let result = Rc::new(paths);
+    cache.insert((from, to), result.clone());
+    result
+}
+
+fn find_shortest_sequence(
+    s: &[u8],
+    depth: usize,
+    highest: bool,
+    cursors: &mut Vec<u8>,
+    cache: &mut HashMap<(Vec<u8>, usize, u8), usize>,
+    path_cache: &mut HashMap<(u8, u8), Rc<Vec<Vec<u8>>>>,
+) -> usize {
+    let cache_key = (s.to_vec(), depth, cursors[depth]);
+    if let Some(cached) = cache.get(&cache_key) {
+        return *cached;
+    }
+
+    let mut result = 0;
+    for &c in s {
+        let paths = find_shortest_paths(
+            if highest { &NUMERIC } else { &DIRECTIONAL },
+            cursors[depth],
+            c,
+            path_cache,
+        );
+        if depth == 0 {
+            result += paths.iter().map(|l| l.len()).min().unwrap();
+        } else {
+            result += paths
+                .iter()
+                .map(|p| find_shortest_sequence(p, depth - 1, false, cursors, cache, path_cache))
+                .min()
+                .unwrap();
         }
-        (false, false) => {
-            // Moving right and down
-            result.extend(vec![Action::Right; dx.unsigned_abs()]);
-            result.extend(vec![Action::Down; dy.unsigned_abs()]);
-        }
+        cursors[depth] = c;
     }
-    result.push(Action::Activate);
+
+    cache.insert(cache_key, result);
 
     result
-}
-
-fn possible_actions_for_buttons(buttons: &[Button]) -> Vec<Vec<Action>> {
-    let mut current = Button::Activate;
-    let mut result: Vec<Vec<Action>> = vec![];
-    for button in buttons {
-        let actions = ways_to_reach_button(&current, button);
-        // We need to combine with all of the previous actions
-        result = result
-            .iter()
-            .flat_map(|prev| {
-                actions.iter().map(move |next| {
-                    let mut result = prev.clone();
-                    result.extend(next.clone());
-                    result
-                })
-            })
-            .collect();
-        current = *button;
-    }
-    result
-}
-
-fn actions_for_buttons(buttons: &[Button]) -> Vec<Action> {
-    let mut result = vec![];
-    for window in buttons.windows(2) {
-        result.extend(actions_to_next_digit(&window[0], &window[1]));
-    }
-    result
-}
-
-fn actions_for_actions(actions: &[Action]) -> Vec<Action> {
-    let mut result = vec![];
-    let mut actions_including_start = vec![Action::Activate];
-    actions_including_start.extend(actions);
-    for window in actions_including_start.windows(2) {
-        result.extend(actions_for_next_action(&window[0], &window[1]));
-    }
-    result
-}
-
-fn buttons_for_input(line: &str) -> Vec<Button> {
-    let mut result = vec![Button::Activate]; // Start over the activate button
-    for digit in line.chars() {
-        result.push(match digit {
-            '1' => Button::One,
-            '2' => Button::Two,
-            '3' => Button::Three,
-            '4' => Button::Four,
-            '5' => Button::Five,
-            '6' => Button::Six,
-            '7' => Button::Seven,
-            '8' => Button::Eight,
-            '9' => Button::Nine,
-            '0' => Button::Zero,
-            'A' => Button::Activate,
-            _ => panic!("Invalid digit"),
-        });
-    }
-    result
-}
-
-fn final_actions_for_digits(digits: &[Button]) -> Vec<Action> {
-    let actions = actions_for_buttons(digits);
-    let actions = actions_for_actions(&actions);
-    actions_for_actions(&actions)
-}
-
-fn shortest_actions_for_digits(digits: &[Button]) -> usize {
-    let actions = possible_actions_for_buttons(digits);
-    // Expand out all of the possible actions into keypad sequences
-    actions
-        .iter()
-        .map(|actions| actions_for_actions(actions))
-        .map(|actions| actions_for_actions(&actions))
-        .map(|actions| actions.len())
-        .min()
-        .unwrap()
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let mut score = 0;
-    for line in input.lines() {
-        let digits = buttons_for_input(line);
-        let num_actions = shortest_actions_for_digits(&digits);
-        let number = line[..line.len() - 1].parse::<usize>().unwrap();
-        score += number * num_actions;
+    let lines = input.lines().collect::<Vec<_>>();
+
+    let mut cache = HashMap::new();
+    let mut path_cache = HashMap::new();
+
+    let max_depth = 2;
+
+    let mut total = 0;
+    for l in &lines {
+        let mut cursors = vec![b'A'; max_depth + 1];
+        let len = find_shortest_sequence(
+            l.as_bytes(),
+            max_depth,
+            true,
+            &mut cursors,
+            &mut cache,
+            &mut path_cache,
+        );
+
+        let n = l[0..3].parse::<usize>().unwrap();
+        total += n * len;
     }
-    Some(score)
+    Some(total)
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let lines = input.lines().collect::<Vec<_>>();
+
+    let mut cache = HashMap::new();
+    let mut path_cache = HashMap::new();
+
+    let max_depth = 25;
+
+    let mut total = 0;
+    for l in &lines {
+        let mut cursors = vec![b'A'; max_depth + 1];
+        let len = find_shortest_sequence(
+            l.as_bytes(),
+            max_depth,
+            true,
+            &mut cursors,
+            &mut cache,
+            &mut path_cache,
+        );
+
+        let n = l[0..3].parse::<usize>().unwrap();
+        total += n * len;
+    }
+    Some(total)
 }
 
 #[cfg(test)]
